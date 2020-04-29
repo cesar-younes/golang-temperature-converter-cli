e79221acda27743e816220ddbc95cd173e2a0cdd
Cesar Younes <cesar.younes@outlook.com>
2020-04-29 17:48:42 +0400

    Ask to convert again

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
 
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
